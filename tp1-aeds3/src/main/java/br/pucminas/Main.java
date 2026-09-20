50d6793c3ab281292e7fca2e7da2411ed7d04e79
Rian Chaves Pimenta <rcpimenta@sga.pucminas.br>
2026-09-20 09:36:22 -0300

    atualizando branch

--- tp1-aeds3/src/main/java/br/pucminas/Main.java
+++ tp1-aeds3/src/main/java/br/pucminas/Main.java
@@ -3,26 +3,11 @@ package br.pucminas;
 import br.pucminas.menu.Menu;
 import br.pucminas.repository.BancoDados;
 
-<<<<<<< HEAD
-import java.io.IOException;
-=======
->>>>>>> 9dcc73d58fb43770a8a2f588cbcbe6df9f12c31b
 import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Path diretorio = Path.of(System.getProperty("ajudaai.data", "data"));
-        try (BancoDados banco = new BancoDados(diretorio);
-             Scanner entrada = new Scanner(System.in)) {
-            new Menu(banco, entrada).executar();
-        } catch (IOException e) {
-            System.err.println("Não foi possível iniciar o banco de dados: " + e.getMessage());
-        }
-    }
-}
-=======
         Path diretorio = Path.of(System.getProperty("ajudaai.data", "dados"));
         try (BancoDados banco = new BancoDados(diretorio);
              Scanner entrada = new Scanner(System.in)) {
@@ -32,4 +17,3 @@ public class Main {
         }
     }
 }
->>>>>>> 9dcc73d58fb43770a8a2f588cbcbe6df9f12c31b
